22b733d55ffbf84bee27e02d2ca4985601487b9e
nikodem-pol4k <nikodem.polak@edu.uekat.pl>
2025-04-26 12:50:28 +0200

    lab 12 exercises

--- main.py
+++ main.py
@@ -1,53 +1,39 @@
 import cv2
-import numpy as np
+
+image = cv2.imread("bird.jpg")
 
 
 def zad1():
-    image = cv2.imread("dwight.jpg")
-    cv2.imshow("Original", image)
-
-    mask = np.zeros(image.shape[:2], dtype="uint8")
-    cv2.rectangle(mask, (260, 5), (450, 240), 255, -1)
-
-    masked = cv2.bitwise_and(image, image, mask=mask)
-    cv2.imshow("Mask Applied to Image", masked)
-
+    cv2.imshow("Zad1", image)
+    (B, G, R) = cv2.split(image)
+    cv2.imshow('Blue Channel', B)
+    cv2.imshow('Green Channel', G)
+    cv2.imshow('Red Channel', R)
     cv2.waitKey()
+
 
 def zad2():
-    image = cv2.imread("dwight.jpg")
-    cv2.imshow("Original", image)
-    mask = np.zeros(image.shape[:2], dtype="uint8")
-
-    top_left = (260, 90)
-    bottom_right = (380, 140)
-    cv2.rectangle(mask, top_left, bottom_right, 255, -1)
-    inverted_mask = cv2.bitwise_not(mask)
-
-    masked = cv2.bitwise_and(image, image, mask=inverted_mask)
-    cv2.imshow("Mask Applied to Image", masked)
-
-    cv2.waitKey()
+    ...
 
 
 def zad3():
-    image = cv2.imread('dwight.jpg')
+    ...
 
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    lower_red = np.array([0, 120, 70])
-    upper_red = np.array([10, 255, 255])
+def zad4():
+    ...
 
-    mask = cv2.inRange(hsv_image, lower_red, upper_red)
 
-    result = cv2.bitwise_and(image, image, mask=mask)
+def zad5():
+    ...
 
-    cv2.imshow('Original Image', image)
-    cv2.imshow('Masked Image', result)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def zad6():
+    ...
 
-# zad1()
+zad1()
 # zad2()
-zad3()
+# zad3()
+# zad4()
+# zad5()
+# zad6()
